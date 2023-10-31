84ddeaeb8ec5d745e6b40f1d6b0a4d0597717830
Cayo Henrique Mercadante <cayo.veiga54@gmail.com>
2023-10-30 23:47:37 -0300

    especificações teste

---
+++ script/script.js
@@ -0,0 +1,32 @@
+function aki(){
+    document.getElementById('aki').style.background = 'url(../img/bg_aki.jpg)';
+    document.getElementById('aki-status').style.visibility = 'visible';
+    document.getElementById("aki-status-mid").style.visibility = 'visible';
+    document.getElementById("aki-status-bot").style.visibility = 'visible';
+    document.getElementById("aki").onmouseenter = function(){
+        this.style.background = 'url(../img/bg_aki.jpg)';
+    }
+    document.getElementById("aki").onmouseleave = function(){
+        this.style.background = 'url(../img/bg_aki.jpg)';
+    }
+}
+
+function desaperecer(){
+    document.getElementById('aki').style.background = 'url(../img/aki-hover.png)';
+    document.getElementById('aki').style.backgroundSize = '68rem';
+    document.getElementById('aki').style.backgroundPosition = 'center';
+    document.getElementById("aki-status").style.visibility = 'hidden';
+    document.getElementById("aki-status-mid").style.visibility = 'hidden';
+    document.getElementById("aki-status-bot").style.visibility = 'hidden';
+    document.getElementById("aki").onmouseenter = function(){
+        this.style.background = 'url(../img/aki.jpg)';
+        document.getElementById('aki').style.backgroundSize = '68rem';
+        document.getElementById('aki').style.backgroundPosition = 'center';
+    };
+    document.getElementById("aki").onmouseleave = function(){
+        this.style.background = "url(../img/aki-hover.png)";
+        document.getElementById('aki').style.backgroundSize = '68rem';
+        document.getElementById('aki').style.backgroundPosition = 'center';
+    };
+}
+
